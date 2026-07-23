c6ddcf8ca7c8fb23bd65849a32c587ffb4f98c40
alireza375 <alirezaraju8@gmail.com>
2026-07-23 22:11:14 +0600

    import blog json file and image and set up prop type in react

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
 
@@ -11,8 +12,11 @@ const Blogs = () => {
     }, [])
 
     return (
-        <div>
-            
+        <div className="md:w-2/3">
+            <h1 className="text-4xl">Blogs: {blogs.length}</h1>
+            {
+                blogs.map(blogs => <Blog key={blog.id} blog={blog} />)
+            }
         </div>
     );
 };
