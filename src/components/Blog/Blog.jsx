bb02a6d3c4953f18b247f0ff3bb88f76338251e0
alireza375 <alirezaraju8@gmail.com>
2026-07-25 09:58:39 +0600

    single blog show

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,10 +1,27 @@
 import PropTypes from 'prop-types';
 
-const Blog = (blog) => {
-    console.log(blog)
+const Blog = ({ blog }) => {
+    const {title, cover, author, author_img, reading_time, 
+        posted_date, hashtags} = blog;
     return (
         <div>
-            
+            <img src={cover} alt='' />
+            <div className='flex justify-between'>
+                <div className='flex'>
+                    <img className='w-14' src={author_img} alt='' />
+                    <div className='ml-6'>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time}</span>
+                </div>
+            </div>
+            <h2 className="text-4xl">{title}</h2>
+            {
+                hashtags.map((hash, idx) => <spam key={idx}><a href="">#{hash}</a></spam>)
+            }
         </div>
     );
 };
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -15,7 +15,7 @@ const Blogs = () => {
         <div className="md:w-2/3">
             <h1 className="text-4xl">Blogs: {blogs.length}</h1>
             {
-                blogs.map(blogs => <Blog key={blog.id} blog={blog} />)
+                blogs.map(blog => <Blog key={blog.id} blog={blog} />)
             }
         </div>
     );
